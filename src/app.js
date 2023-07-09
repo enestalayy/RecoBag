ddd7e79ea7d9991dd75a8008e10266790f596342
enestalayy <enestalayy@gmail.com>
2023-07-09 19:02:20 +0300

    Translater fixed

--- src/app.js
+++ src/app.js
@@ -1,4 +1,12 @@
+ src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
 
+function googleTranslateElementInit(){
+    new google.translate.TranslateElement(
+        {pageLanguage: 'tr'},
+        'google_translate_element'
+    );
+}
+  
 
 
 
