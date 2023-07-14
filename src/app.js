bd2be1924d1dcd5436b529eddc9064c0d78726f7
enestalayy <enestalayy@gmail.com>
2023-07-14 17:34:06 +0300

    fix

--- src/app.js
+++ src/app.js
@@ -1,12 +1,3 @@
- src="https://translate.google.com/translate_a/element.js?cb=googleTranslateElementInit"
-
-function googleTranslateElementInit(){
-    new google.translate.TranslateElement(
-        {pageLanguage: 'tr'},
-        'google_translate_element'
-    );
-}
-  
 
 
 
